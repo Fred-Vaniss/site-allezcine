e1c8eb3c06a3502bcbc69b26e3d736e6e89f6cf5
Balthazar Delvaux <Delvaux.balthazar@gmail.com>
2019-06-18 15:16:39 +0200

    fin popup check age

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+// Affiche la popup qui check l'age au chargement de la page
+$(window).on('load', () => {
+    $('#myModal').modal('show');
+});
+
+// Validation de la popup qui check l'age
+const validate = document.getElementById('validate');
+validate.addEventListener('click', () => {
+    const dateField = document.getElementById('inputDate').value;
+    const dateSplit = dateField.split('-');
+    const day = dateSplit[2];
+    const month = dateSplit[1];
+    const year = dateSplit[0];
+    const date = new Date();
+    const yearNow = date.getFullYear();
+    const monthNow = date.getMonth() + 1;
+    const dayNow = date.getDate();
+    let age = 0;
+    age = yearNow - year;
+    if (monthNow >= month) {
+        if (dayNow > day) {
+            age--;
+        }
+    }
+    if (dateField != "") {
+        if (age >= 18) {
+                $('#myModal').modal('toggle');
+        } else if (age < 18) {
+            window.location.replace("https://www.imdb.com/");
+        } else {
+            console.log('erreur');
+        }
+    }
+
+})
