d8e56c72e0cb5cc77f4d8573adca7302336a0552
Antoine <antoine.notte1@gmail.com>
2019-06-19 11:33:57 +0200

    formulaire contact

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,32 @@
-let films = {};
-let req = new XMLHttpRequest();
-req.open("GET", "https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2014-09-15&primary_release_date.lte=2014-10-22&api_key=3b4cac2f6fd40d51e8ffc2881ade3885", true)
-req.onreadystatechange = function () {
-  if (req.readyState === XMLHttpRequest.DONE){
-      if(req.status == 200){
-          films = JSON.parse(req.responseText);
-          console.log(films)
-      } else {
-          console.error(`Erreur ${req.status}`)
-      }
-  }
-};
-req.send();
+//Vérification password
+let passeone = document.querySelector('#Password1');
+let passetwo = document.querySelector('#Password2');
+
+document.querySelector('.butt-enregistrer').addEventListener('click', () => {
+    if (passeone !== passetwo) {
+        passeone.style.borderColor = "red";
+        passetwo.style.borderColor = "red";
+    }
+})
+
+// Validation formulaire//
+let confirmname = document.querySelector('#confirm-name')
+let confirmfirstname = document.querySelector("#confirm-firstname")
+let confirmmail = document.querySelector("#confirm-mail")
+let confirmobject = document.querySelector("#confirm-object")
+let confirmmess = document.querySelector("#confirm-mess")
+
+
+let confirm = document.querySelector('#envoie').addEventListener('click', () => {
+    let name = document.querySelector('#cname').value
+    let firstname = document.querySelector('#cfirstname').value
+    let mail = document.querySelector('#cmail').value
+    let object = document.querySelector('#cobject').value
+    let message = document.querySelector('#cmessage').value
+    confirmname.value = name
+    confirmfirstname.value = name
+    confirmmail.value = mail
+    confirmobject.value = object
+    confirmmess.value = message
+    console.log(name)
+})
