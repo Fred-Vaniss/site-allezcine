8082834092542e298281b91878a65c4d72b89b44
user <maraisloic1208@gmail.com>
2019-06-19 14:48:21 +0200

    creation du footer

---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// Création du bouton
+let createbtn = document.createElement("A");
+let txt = document.createTextNode("↑");
+createbtn.setAttribute('id', 'btnscroll');
+createbtn.setAttribute('href', '#up');
+createbtn.appendChild(txt);
+document.body.appendChild(createbtn);
+let btnscroll = document.getElementById("btnscroll");
+
+//Fonction qui fait apparaitre et disparaitre le bouton en fonction du scroll
+document.addEventListener('DOMContentLoaded', function () {
+    window.onscroll = (function (event) {
+        if (window.pageYOffset >= 220) {
+            btnscroll.style.display = "inline";
+        } else {
+            btnscroll.style.display = "none";
+        };
+    });
+});
